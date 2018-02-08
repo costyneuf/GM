0d4ad3062c4b2380e7b362d3a4f51a3367f1ccd4
costyneuf <youzi_g@163.com>
2018-02-08 04:47:31 -0500

    Create new class for De Casteljau

---
+++ Geometric_Modeling_Lab1/src/curve/SubdivisionDeCasteljau.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package curve;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.swt.graphics.Point;
+
+/**
+ * @author gaoxing
+ *
+ */
+public class SubdivisionDeCasteljau {
+
+    private List<Point> ctrlPoints = new LinkedList<>();
+
+    public SubdivisionDeCasteljau(List<Point> ctrlPoints, int subdivisions) {
+
+    }
+
+}
