bf2aaf0d88a9ec844452f1f4feeb0a0396dcc80c
costyneuf <youzi_g@163.com>
2018-02-07 20:14:48 -0500

    begin a new project

--- Geometric_Modeling_Lab1/src/MainClass.java
+++ Geometric_Modeling_Lab1/src/MainClass.java
@@ -1,21 +1,26 @@
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.SystemColor;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
-import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+
+import org.eclipse.swt.graphics.Point;
 
 public class MainClass {
 
+    private Curve curve = new Curve();
     private JFrame frame;
     private JTextField textField;
+    private ButtonGroup curveChoice = new ButtonGroup();
+    private ButtonGroup pointChoice = new ButtonGroup();
 
     /**
      * Launch the application.
@@ -45,85 +50,120 @@ public class MainClass {
      * Initialize the contents of the frame.
      */
     private void initialize() {
-        this.frame = new JFrame();
+        this.frame = new JFrame("CSE 5543 Geometric Modeling - LAB 1");
         this.frame.setBounds(100, 100, 1264, 847);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.getContentPane().setLayout(null);
 
-        JButton Tech_Bezier_Curve = new JButton("Bezier Curve");
-        Tech_Bezier_Curve.setBounds(34, 57, 277, 25);
-        this.frame.getContentPane().add(Tech_Bezier_Curve);
-
-        JButton Tech_UniCubic_BSpline = new JButton("Uniform Cubic B-Spline");
-        Tech_UniCubic_BSpline.setBounds(34, 116, 277, 25);
-        this.frame.getContentPane().add(Tech_UniCubic_BSpline);
-
-        JButton Tech_UniQua_BSpline = new JButton("Uniform Quadratic B-Spline");
-        Tech_UniQua_BSpline.setBounds(34, 170, 277, 25);
-        this.frame.getContentPane().add(Tech_UniQua_BSpline);
-
-        JButton Tech_Closed_Quadratic_BSpline = new JButton(
-                "Closed Uniform Quadratic B-Spline");
-        Tech_Closed_Quadratic_BSpline.setBounds(34, 230, 277, 25);
-        this.frame.getContentPane().add(Tech_Closed_Quadratic_BSpline);
-
-        JButton Tech_Closed_Cubic_BSpline = new JButton(
-                "Closed Uniform Cubic B-Spline");
-        Tech_Closed_Cubic_BSpline.setBounds(34, 292, 277, 25);
-        this.frame.getContentPane().add(Tech_Closed_Cubic_BSpline);
-
         JTextPane Text1 = new JTextPane();
         Text1.setBackground(UIManager.getColor("Button.background"));
         Text1.setText("Subdivision Iterations:");
-        Text1.setBounds(34, 342, 144, 21);
+        Text1.setBounds(34, 345, 144, 21);
         this.frame.getContentPane().add(Text1);
 
         this.textField = new JTextField();
-        this.textField.setBounds(187, 338, 65, 55);
+        this.textField.setText("4");
+        this.textField.setToolTipText("");
+        this.textField.setBounds(180, 345, 40, 25);
         this.frame.getContentPane().add(this.textField);
         this.textField.setColumns(10);
 
+        /*
+         * Set up a refresh button. The button will parse the input in the
+         * message box to the curve.
+         */
         JButton refreshButton = new JButton("Refresh");
-        refreshButton.setBounds(34, 368, 117, 25);
+        refreshButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    MainClass.this.curve.changeSubdivision(Integer
+                            .parseInt(MainClass.this.textField.getText()));
+
+                } catch (NumberFormatException e1) {
+
+                } catch (NullPointerException e2) {
+
+                }
+            }
+        });
+        refreshButton.setBounds(225, 345, 95, 25);
         this.frame.getContentPane().add(refreshButton);
 
+        /*
+         * Set up a canvas where the points will be input and the curve will be
+         * displayed.
+         */
         Canvas canvas = new Canvas();
+        canvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent arg0) {
+
+                // Add a point into the linked list.
+                PointsOperation.addPoints(MainClass.this.curve,
+                        new Point(arg0.getX(), arg0.getY()),
+                        canvas.getGraphics());
+
+            }
+
+        });
         canvas.setBackground(
                 UIManager.getColor("ComboBox.selectionForeground"));
         canvas.setBounds(364, 57, 869, 715);
         this.frame.getContentPane().add(canvas);
 
-        JMenu editMenu = new JMenu("Edit");
-        editMenu.setHorizontalAlignment(SwingConstants.LEFT);
-        editMenu.setBounds(34, 424, 95, 19);
-        this.frame.getContentPane().add(editMenu);
+        JRadioButton add_point = new JRadioButton("Add points");
+        add_point.setSelected(true);
+        add_point.setBounds(34, 380, 149, 23);
+        this.frame.getContentPane().add(add_point);
 
-        JMenuItem edit_Add = new JMenuItem("Add Points");
-        edit_Add.setSelected(true);
-        editMenu.add(edit_Add);
+        JRadioButton insert_point = new JRadioButton("Insert before selected");
+        insert_point.setBounds(34, 410, 224, 23);
+        this.frame.getContentPane().add(insert_point);
 
-        JMenuItem edit_Insert = new JMenuItem("Insert Before Selected");
-        edit_Insert.setSelected(true);
-        edit_Insert.setBackground(SystemColor.inactiveCaptionBorder);
-        editMenu.add(edit_Insert);
+        JRadioButton edit_point = new JRadioButton("Edit points");
+        edit_point.setBounds(34, 440, 149, 23);
+        this.frame.getContentPane().add(edit_point);
 
-        JMenuItem edit_Edit = new JMenuItem("New menu item");
-        edit_Edit.setSelected(true);
-        editMenu.add(edit_Edit);
+        JButton duplicate = new JButton("Duplicate at selected");
+        duplicate.setBounds(34, 480, 277, 25);
+        this.frame.getContentPane().add(duplicate);
 
-        JMenu curveMenu = new JMenu("Curve");
-        curveMenu.setHorizontalAlignment(SwingConstants.LEFT);
-        curveMenu.setBounds(34, 491, 95, 19);
-        this.frame.getContentPane().add(curveMenu);
+        JButton delete = new JButton("Delete selected");
+        delete.setBounds(34, 520, 277, 25);
+        this.frame.getContentPane().add(delete);
 
-        JMenuItem curve_Duplicate = new JMenuItem("Duplicate Selected Point");
-        curveMenu.add(curve_Duplicate);
+        JButton clear = new JButton("Clear");
+        clear.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent arg0) {
+                MainClass.this.curve.clear();
+                Graphics2D g2 = (Graphics2D) canvas.getGraphics();
+                g2.setColor(UIManager.getColor("ComboBox.selectionForeground"));
+                g2.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            }
+        });
+        clear.setBounds(34, 560, 277, 25);
+        this.frame.getContentPane().add(clear);
 
-        JMenuItem curve_Delete = new JMenuItem("Delete Selected Point");
-        curve_Delete.setBackground(Color.LIGHT_GRAY);
-        curveMenu.add(curve_Delete);
+        JRadioButton uniform_sub_quadric_bSpline = new JRadioButton(
+                "Uniform Subdivision Quadric B-Spline");
+        uniform_sub_quadric_bSpline.setBounds(34, 270, 324, 23);
+        this.frame.getContentPane().add(uniform_sub_quadric_bSpline);
 
-        JMenuItem curve_Remove = new JMenuItem("Remove All");
-        curveMenu.add(curve_Remove);
+        JRadioButton deCasteljau_sub_curve = new JRadioButton(
+                "de Casteljau Subdivision Curve");
+        deCasteljau_sub_curve.setBounds(34, 210, 324, 23);
+        this.frame.getContentPane().add(deCasteljau_sub_curve);
+
+        JRadioButton uniform_cubic_bSpline = new JRadioButton(
+                "Uniform Cubic B-Spline");
+        uniform_cubic_bSpline.setBounds(34, 150, 324, 23);
+        this.frame.getContentPane().add(uniform_cubic_bSpline);
+
+        JRadioButton bezier_curve = new JRadioButton("Bezier Curve");
+        bezier_curve.setSelected(true);
+        bezier_curve.setBounds(34, 90, 324, 23);
+        this.frame.getContentPane().add(bezier_curve);
     }
 }
