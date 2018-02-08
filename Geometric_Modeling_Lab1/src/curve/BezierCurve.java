6259435093f9d22472b1ace81a0013a73b616f08
costyneuf <youzi_g@163.com>
2018-02-08 01:11:47 -0500

    Finish Bezier Curve Class

--- Geometric_Modeling_Lab1/src/curve/BezierCurve.java
+++ Geometric_Modeling_Lab1/src/curve/BezierCurve.java
@@ -3,6 +3,9 @@
  */
 package curve;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,17 +21,12 @@ public class BezierCurve {
      * Private members.
      */
 
-    private static final double INCREMENT = 0.00001;
+    private static final double INITIAL_U = 1E-2;
 
     /**
      * Store a copy of ctrlPoints list.
      */
     private List<Point> points = new LinkedList<>();
-
-    /**
-     * Store points for generating Bezier Curves.
-     */
-    private List<Point> functionPoints = new LinkedList<>();
 
     /**
      * Constructor of BezierCurve.
@@ -44,11 +42,6 @@ public class BezierCurve {
             this.points.add(ctrlPoints.get(i));
         }
 
-        /*
-         * Set up the start and end points.
-         */
-        this.functionPoints.add(0, ctrlPoints.get(0));
-        this.functionPoints.add(1, ctrlPoints.get(ctrlPoints.size() - 1));
     }
 
     /*
@@ -61,8 +54,13 @@ public class BezierCurve {
      * @param n
      * @return 1 * 2 * 3 * ... * n
      */
-    private static int factorial(int n) {
-        int fact = 1;
+    private static long factorial(int n) {
+
+        /*
+         * if n = 0, then factorial(0) = 1
+         */
+        long fact = 1;
+
         for (int i = 1; i <= n; i++) {
             fact *= i;
         }
@@ -75,14 +73,53 @@ public class BezierCurve {
      * @param n
      *            degree of Bernstein polynomial
      * @param i
-     *
+     * @param u
      * @return the ith value
      */
-    private static int bernstein(int n, int i) {
-        return factorial(n) / (factorial(n - i) * factorial(i));
+    private static double bernstein(int n, int i, double u) {
+
+        double result = 1.0;
+        result *= (factorial(n) / factorial(n - i) / factorial(i));
+        result *= Math.pow(u, i);
+        result *= Math.pow(1.0 - u, n - i);
+        return result;
     }
 
     /*
      * Public methods.
      */
+
+    public void generateCurve(Graphics g) {
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.WHITE);
+
+        int p1X = this.points.get(0).x;
+        int p1Y = this.points.get(0).y;
+        int n = this.points.size();
+
+        if (n > 2) {
+
+            double j = INITIAL_U;
+
+            while (j < 1) {
+
+                double p2X = 0, p2Y = 0;
+
+                for (int i = 0; i < n; i++) {
+                    p2X += (this.points.get(i).x * bernstein(n - 1, i, j));
+                    p2Y += (this.points.get(i).y * bernstein(n - 1, i, j));
+                }
+
+                g2.drawLine(p1X, p1Y, (int) Math.round(p2X),
+                        (int) Math.round(p2Y));
+                p1X = (int) Math.round(p2X);
+                p1Y = (int) Math.round(p2Y);
+                j += INITIAL_U;
+            }
+        }
+        g2.drawLine(p1X, p1Y, this.points.get(n - 1).x,
+                this.points.get(n - 1).y);
+
+    }
 }
